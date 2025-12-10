0c24721310bbe7df00ae3f050d238a433aaa12a6
spe-lilmay <spewanyama@gmail.com>
2025-12-10 22:39:41 +0300

    Area

---
+++ Area.py
@@ -0,0 +1,5 @@
+length=int(input("Enter the length: "))
+width=int(input("now enter the width "))
+ #perform multiplication
+Area = length * width 
+print(f"the area of the triangle with {length} and {width} is {Area}")
